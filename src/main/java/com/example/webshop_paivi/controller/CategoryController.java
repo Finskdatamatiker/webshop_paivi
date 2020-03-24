da8a2c47a90c11b30c0cf7ef2b582a9d24631a7b
Päivi Eversbusch <paiv0018@stud.kea.dk>
2020-03-24 12:46:51 +0100

    Kategori-view

--- src/main/java/com/example/webshop_paivi/controller/CategoryController.java
+++ src/main/java/com/example/webshop_paivi/controller/CategoryController.java
@@ -6,8 +6,12 @@ import com.example.webshop_paivi.model.Product;
 import com.example.webshop_paivi.service.category.ICategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -26,4 +30,46 @@ public class CategoryController {
         model.addAttribute("produkterI", listeProdukterICategory);
         return "/category/categoryForside";
     }
+
+
+    @GetMapping("/createCategory")
+    public String visCreate(Category category, Model model){
+        model.addAttribute("category", category);
+        return "/category/createCategory";
+    }
+
+    @PostMapping("/createCategory")
+    public String lavCompany(@Valid Category category, BindingResult bindingResult, Model model)
+    {
+        if(bindingResult.hasErrors()){
+            model.addAttribute("bindingResult", bindingResult);
+            return "/category/createCategory";
+        }
+
+        iCategoryService.gem(category);
+        return "redirect:/categoryForside";
+    }
+
+    @GetMapping("/updateCategory/{id}")
+    public String visUpdate(@PathVariable("id") long id, Model model){
+        model.addAttribute("category", iCategoryService.findMedId(id));
+        return "/category/updateCategory";
+    }
+
+    @PostMapping("/updateCategory")
+    public String update(@Valid Category category, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("bindingResult", bindingResult);
+            return "/category/updateCategory";
+        }
+        iCategoryService.gem(category);
+        return "redirect:/categoryForside";
+    }
+
+    @GetMapping("/deleteCategory/{id}")
+    public String delete(@PathVariable("id") long id){
+        iCategoryService.slet(id);
+        return "redirect:/categoryForside";
+    }
+
 }
